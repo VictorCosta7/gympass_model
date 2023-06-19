eb6efcc5305b013380611c4714ae2abe6b1ce501
VictorCosta7 <engvictorcosta10@gmail.com>
2023-06-19 15:55:57 -0300

    fetch-user-check-list

---
+++ src/http/use-cases/get-user-metrics.ts
@@ -0,0 +1,27 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "../repositories/check-ins-repository";
+
+
+interface GetUserMetricsUseCaseRequest {
+    userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+    checkInsCounts: number
+}
+
+export class GetUserMetricsUseCase {
+    constructor(
+        private checkInsRepository: CheckInsRepository
+    ) { }
+
+    async execute({
+        userId,
+    }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+        const checkInsCounts = await this.checkInsRepository.countByUseId(userId)
+
+        return {
+            checkInsCounts
+        }
+    }
+}

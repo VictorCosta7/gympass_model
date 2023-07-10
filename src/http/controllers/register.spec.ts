f4f08487b28cd090557f1f90f79018a6c7e285b4
VictorCosta7 <engvictorcosta10@gmail.com>
2023-07-10 10:23:24 -0300

    test_e2e_register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,25 @@
-import { test } from "vitest";
+import request from 'supertest';
+import { app } from '@/app';
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test('ok', () => { })
+describe('Registe (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('shold be able to register', async () => {
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name: "Victor Costa",
+                email: "victor@email.com",
+                password: '123456'
+            })
+
+        expect(response.statusCode).toEqual(201)
+    })
+})
